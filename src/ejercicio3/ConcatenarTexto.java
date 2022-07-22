1e9448bc91dbe05499dda338f493cd93b9a037ed
AlbertoD11 <95533052+AlbertoD11@users.noreply.github.com>
2022-07-22 18:38:15 +0200

    ejercio3

---
+++ src/ejercicio3/ConcatenarTexto.java
@@ -0,0 +1,17 @@
+package ejercicio3;
+
+public class ConcatenarTexto {
+
+
+    public static void main(String[] args){
+
+        String[] nombres = {"Seat", "Volvo", "Mercedes","Audi"};
+        for(int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+
+    }
+
+
+
+}
